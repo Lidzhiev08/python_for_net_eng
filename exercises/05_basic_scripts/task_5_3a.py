0e14a78c67d92dcdfbbfa07028d7541658e1e670
Lidzhiev08 <lidzhiev_08_03@mail.ru>
2024-07-24 13:00:49 +0300

    5.3a done

--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -25,3 +25,13 @@ trunk_template = [
     "switchport mode trunk",
     "switchport trunk allowed vlan {}",
 ]
+
+temp = {'access': access_template, 'trunk': trunk_template}
+temp_ques = {'access': 'Enter number of VLAN:', 'trunk': 'Enter enable VLANs:'}
+regime = input('Enter regime of interface work (access/trunk): ')
+type_num = input('Enter type and number of interface: ')
+vlans = input(temp_ques.get(regime))
+
+print('interface {}'.format(type_num))
+str1 = '\n'.join(temp.get(regime))
+print(str1.format(vlans))
