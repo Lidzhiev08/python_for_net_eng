3e5079cbc7c9f1b72a0d90e909c0d5f26c6f6e84
Lidzhiev08 <lidzhiev_08_03@mail.ru>
2024-07-25 15:18:27 +0300

    6.2 done

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -12,3 +12,21 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip = input('Enter IP address in format 10.0.1.1: ')
+
+octs = ip.split('.')
+new_list = []
+for oct in octs:
+    new_list.append(int(oct))
+
+if  new_list[0] >= 1 and  new_list[0] <= 223:
+    print('unicast')
+elif  new_list[0] >= 224 and  new_list[0] <= 239:
+    print('multicast')
+elif  new_list[0] == 255 and  new_list[1] == 255 and  new_list[2] == 255 and  new_list[3] == 255:
+    print('local broadcast')
+elif  new_list[0] == 0 and  new_list[1] == 0 and  new_list[2] == 0 and  new_list[3] == 0:
+    print('unassigned')
+else:
+    print('unused')
