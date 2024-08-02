d7aa1b665b782d0cfe9d1edfead4ae985d084137
Lidzhiev08 <lidzhiev_08_03@mail.ru>
2024-08-02 16:04:10 +0300

    7.1 done

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,3 +14,19 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+with open('ospf.txt') as f:
+    for line in f:
+        list_line = line.split()
+        list_line.pop(0)
+        list_line.pop(2)
+        list_line[1] = list_line[1].strip('[]')
+        list_line[2] = list_line[2].rstrip(',')
+        list_line[3] = list_line[3].rstrip(',')
+        pr, ad, next, last, out = list_line
+        print(f'{'Prefix':20}{pr}')
+        print(f'{'AD/Metric':20}{ad}')
+        print(f'{'Next-Hop':20}{next}')
+        print(f'{'Last update':20}{last}')
+        print(f'{'Outbound Interface':20}{out}')
+        print('\n')
