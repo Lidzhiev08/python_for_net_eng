1ae20c5a393797af81423fbb137ba0027c43bdab
Lidzhiev08 <lidzhiev_08_03@mail.ru>
2024-08-07 19:23:55 +0300

    9.1 done

--- exercises/09_functions/task_9_1.py
+++ exercises/09_functions/task_9_1.py
@@ -75,3 +75,20 @@ def generate_access_config(intf_vlan_mapping, access_template):
 
     Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
     """
+    result = []
+    for intf, vlan in intf_vlan_mapping.items():
+        result.append(f'interface {intf}')
+        for command in access_template:
+            if command.endswith('access vlan'):
+                result.append(f'{command} {vlan}')
+            else:
+                result.append(f'{command}')
+    return result
+
+access_list_1 = generate_access_config(access_config, access_mode_template)
+print(access_list_1)
+
+print('-' * 40, '\n')
+
+access_list_2 = generate_access_config(access_config_2, access_mode_template)
+print(access_list_2)
