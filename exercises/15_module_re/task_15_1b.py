560daaf4c49bc64efced57ba2e9093970dc9352c
Lidzhiev08 <lidzhiev_08_03@mail.ru>
2024-09-06 20:48:40 +0300

    15.1b done

--- exercises/15_module_re/task_15_1b.py
+++ exercises/15_module_re/task_15_1b.py
@@ -28,3 +28,34 @@ IP-адреса, диапазоны адресов и так далее, так 
 а не ввод пользователя.
 
 """
+
+import re
+def get_ip_from_cfg(filename):
+    res_dict = {}
+    list_keys = []
+    list_values = []
+    regex_intf = (r'interface (?P<intf>\S+)')
+    regex = (r'ip address (?P<ip_addr>(\d+\.)+\S+) (?P<mask>(\d+\.)+\S+)')
+    with open(filename) as f:
+        for line in f:
+            match_intf = re.search(regex_intf, line)
+            if match_intf:
+                list_keys.append(match_intf.group('intf'))
+                continue
+            match = re.search(regex, line)
+            if 'secondary' in line:
+                list_values[-1] = [list_values[-1][0], (match.group('ip_addr'), match.group('mask'))]
+                continue
+            if match:
+                temp_tuple = (match.group('ip_addr'), match.group('mask'))
+                list_values.append([temp_tuple])
+                temp_tuple = []
+                continue
+            if 'no ip address' in line or 'ip unnumbered' in line:
+                del_elem = list_keys.pop()
+            res_dict = dict(zip(list_keys, list_values))
+    return res_dict
+
+if __name__ == '__main__':
+    test_list = get_ip_from_cfg('config_r2.txt')
+    print(test_list)
