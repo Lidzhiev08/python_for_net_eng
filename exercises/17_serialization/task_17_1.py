985950afd6740d708c8d936c737e785b6f480137
Lidzhiev08 <lidzhiev_08_03@mail.ru>
2024-09-21 15:12:45 +0300

    17.1 done

--- exercises/17_serialization/task_17_1.py
+++ exercises/17_serialization/task_17_1.py
@@ -31,3 +31,35 @@ sw3,00:E9:22:11:A6:50,100.1.1.7,3,FastEthernet0/21
 sw2_dhcp_snooping.txt, sw3_dhcp_snooping.txt.
 
 """
+import csv
+import re
+
+def write_dhcp_snooping_to_csv(filenames, output):
+    headers = ['switch','mac','ip','vlan','interface']
+    data = []
+    data.append(headers)
+    regex = r'(?P<mac>(\S+\:)\S+) +(?P<ip>(\S+\.)\S+).+ +(?P<vlan>\d+) +(?P<intf>\S+)'
+    regex_two = r'17_serialization/(?P<device>\S+)_dhcp_snooping.txt'
+    for file in filenames:
+        with open(file) as f:
+            device = re.search(regex_two, file)
+            for line in f:
+                match = re.search(regex, line)
+                if match:
+                    temp = [device.group('device'), match.group('mac'), match.group('ip'), match.group('vlan'), match.group('intf')]
+                    data.append(temp)
+                    temp = []
+    with open(output, 'w', newline='') as csvf:
+        writer = csv.writer(csvf)
+        for row in data:
+            writer.writerow(row)
+
+if __name__ == '__main__':
+    files = ['sw1_dhcp_snooping.txt','sw2_dhcp_snooping.txt', 'sw3_dhcp_snooping.txt']
+    new_files = []
+    for file in files:
+        file = '17_serialization/' + file
+        new_files.append(file)
+    write_dhcp_snooping_to_csv(new_files, '17_serialization/dhcp_snooping.csv')
+    with open('17_serialization/dhcp_snooping.csv') as f:
+        print(f.read())
