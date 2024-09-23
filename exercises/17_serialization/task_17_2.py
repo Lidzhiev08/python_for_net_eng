9e2f28815c1bf4e59f4bda6ba669b85a983c262a
Lidzhiev08 <lidzhiev_08_03@mail.ru>
2024-09-23 20:39:24 +0300

    17.2 done

--- exercises/17_serialization/task_17_2.py
+++ exercises/17_serialization/task_17_2.py
@@ -44,8 +44,72 @@
 """
 
 import glob
+import csv
+import re
 
-sh_version_files = glob.glob("sh_vers*")
+sh_version_files = glob.glob("17_serialization/sh_vers*")
 # print(sh_version_files)
 
 headers = ["hostname", "ios", "image", "uptime"]
+
+def parse_sh_version(out_command):
+    '''
+    на вход подается вывод команды sh version как строка,
+    на выходе кортеж (IOS, image, uptime)
+    '''
+    result = ()
+    temp = []
+    regex_ios = r'.+IOS.+Version (?P<ios>\S+)\,.+'
+    regex_image = r'System image file is (?P<image>\S+)'
+    regex_uptime = r'router uptime is (?P<uptime>.+)'
+    lines = out_command.split('\n')
+    for line in lines:
+        line = line.rstrip()
+        match_ios = re.search(regex_ios, line)
+        match_image = re.search(regex_image, line)
+        match_uptime = re.search(regex_uptime, line)
+        if match_ios:
+            temp.append(match_ios.group('ios'))
+        if match_image:
+            image = match_image.group('image').strip('""')
+            temp.append(image)
+        if match_uptime:
+            temp.append(match_uptime.group('uptime'))
+        result = tuple(temp)
+    return result
+
+def write_inventory_to_csv(data_filenames, csv_filename):
+    '''
+    два параметра:
+    * список имен файлов с выводом sh version
+    * имя файла (например, routers_inventory.csv), в который будет записана информация в формате CSV
+    ничего не возвращает
+
+    Функция делает следующее:
+    * обработать информацию из каждого файла с выводом sh version:
+    * sh_version_r1.txt, sh_version_r2.txt, sh_version_r3.txt
+    * с помощью функции parse_sh_version, из каждого вывода должна быть получена
+    информация ios, image, uptime
+    * из имени файла нужно получить имя хоста
+    * после этого вся информация должна быть записана в CSV файл
+    '''
+    regex = r'17_serialization\\sh_version_(?P<host>\S+).txt'
+    data = []
+    temp = []
+    data.append(headers)
+    for file in data_filenames:
+        host = re.search(regex, file)
+        temp.append(host.group('host'))
+        with open(file) as f:
+            output_data = f.read()
+            out_tuple = list(parse_sh_version(output_data))
+            temp.extend(out_tuple)
+            data.append(temp)
+            temp = []
+    with open(csv_filename, 'w', newline='') as csvf:
+        writer = csv.writer(csvf, quoting=csv.QUOTE_NONNUMERIC)
+        for line in data:
+            writer.writerow(line)
+
+if __name__ == '__main__':
+    write_inventory_to_csv(sh_version_files, '17_serialization/info_versions.csv')
