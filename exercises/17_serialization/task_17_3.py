94e318c18c60f0c9ee0c7124cbf275550d596a56
Lidzhiev08 <lidzhiev_08_03@mail.ru>
2024-09-24 18:55:39 +0300

    17.3 done

--- exercises/17_serialization/task_17_3.py
+++ exercises/17_serialization/task_17_3.py
@@ -24,3 +24,33 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 """
+
+def parse_sh_cdp_neighbors(out_com):
+    result = {}
+    d_keys = []
+    d_values = []
+    temp = {}
+    lines = out_com.split('\n') #list of lines of commands
+    for line in lines:
+        line = line.rstrip()
+        if len(line.split('>')) == 2:
+            host = line.split('>')[0] #name host
+        if line == '':
+            continue
+        if len(line.split()) > 5 and line.split()[3].isdigit():
+            line = line.split()
+            value = line[-2] + ' ' + line[-1] # порт подключенного устройства
+            key = line[1] + ' ' + line[2] #порт хоста
+            temp[line[0]] = value
+            d_values.append(temp)
+            d_keys.append(key)
+            temp = {} #delete dict
+    res_value = dict(zip(d_keys, d_values))
+    result[host] = res_value
+    return result
+
+if __name__ == '__main__':
+    with open('17_serialization/sh_cdp_n_sw1.txt') as f:
+        out_command = f.read()
+        res = parse_sh_cdp_neighbors(out_command)
+        print(res)
