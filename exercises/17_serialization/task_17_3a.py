f041495899b2e9cd2ed2d7fed895584075960e60
Lidzhiev08 <lidzhiev_08_03@mail.ru>
2024-09-25 22:46:07 +0300

    17.3a done

--- exercises/17_serialization/task_17_3a.py
+++ exercises/17_serialization/task_17_3a.py
@@ -36,3 +36,53 @@
 в файл topology.yaml. Он понадобится в следующем задании.
 
 """
+import pprint as pp
+import yaml
+
+def generate_topology_from_cdp(list_of_files, save_to_filename=None):
+    '''
+        обрабатывает вывод команды show cdp neighbor из нескольких файлов и записывает итоговую
+        топологию в один словарь.
+        параметры:
+        * list_of_files - список файлов из которых надо считать вывод команды sh cdp neighbor
+        * save_to_filename - имя файла в формате YAML, в который сохранится топология.
+        * значение по умолчанию - None. По умолчанию, топология не сохраняется в файл
+        * топология сохраняется только, если save_to_filename как аргумент указано имя файла
+    '''
+    result = {}
+    d_keys = []
+    d_values = []
+    temp = {}
+    for file in list_of_files:
+        with open(file) as f:
+            for line in f:
+                if len(line.split('>')) == 2:
+                    host = line.split('>')[0] #name host
+                if line == '':
+                    continue
+                if len(line.split()) > 5 and line.split()[3].isdigit():
+                    line = line.split()
+                    value = line[-2] + ' ' + line[-1] # порт подключенного устройства
+                    key = line[1] + ' ' + line[2] #порт хоста
+                    temp[line[0]] = value
+                    d_values.append(temp)
+                    d_keys.append(key)
+                    temp = {} #delete dict
+            res_value = dict(zip(d_keys, d_values))
+            result[host] = res_value
+            d_keys = []
+            d_values = []
+    with open(save_to_filename, 'w') as f:
+        yaml.dump(result, f)
+    return result
+
+if __name__ == '__main__':
+    files = ['sh_cdp_n_sw1.txt', 'sh_cdp_n_r1.txt', 'sh_cdp_n_r2.txt', 'sh_cdp_n_r3.txt',
+              'sh_cdp_n_r4.txt', 'sh_cdp_n_r5.txt', 'sh_cdp_n_r6.txt']
+    new_list = []
+    for file in files:
+        file = '17_serialization/' + file
+        new_list.append(file)
+
+res = generate_topology_from_cdp(new_list, save_to_filename='17_serialization/topology_from_17_3a.yaml')
+pp.pprint(res)
